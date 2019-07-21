16092379a14c25df522cf4a8af7718a254e3603b
ASSROUGUI <ASSROUGUI@hotmail.com>
2019-07-21 19:26:48 +0300

    added viewer+method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def index
+    @movie = Movie.all 
   end
 
+  def new
+    @movie=Movie.new
+  end
   def show
   end
 
@@ -19,20 +25,40 @@ class MoviesController < ApplicationController
 
   def search
   end
-
-  def delete
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+      
+    redirect_to @movie
   end
-end
 
-def index
-end
-
-def new
-    @movie=Movie.new
-end
+  def destroy
+        @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, :notice=>"your movie has been deleted"
+  end
 
 
-private
-def movie_params
-params.require(:movie).permit(:title, :content)
+
+
+
+
+
+ private
+ def movie_params
+ params.require(:movie).permit(:title, :content)
+ end
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def movie_params
+      params.require(:movie).permit(:movies_name, :description, :movie_length, :actors_name, :rating)
+    end
 end
