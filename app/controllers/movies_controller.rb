b5089c19114a139ad01bf3ee13d3686cb7cf6d07
ASSROUGUI <ASSROUGUI@hotmail.com>
2019-07-21 13:51:35 +0300

    just bulit controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,19 @@
+class MoviesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def search
+  end
+
+  def delete
+  end
+end
