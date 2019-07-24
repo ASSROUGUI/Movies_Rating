79c0c26f40175100fa4862282bda9aabe9efb087
ASSROUGUI <ASSROUGUI@hotmail.com>
2019-07-24 15:43:12 +0300

    delete review done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_review, only: [:show, :destroy]
  
   before_action :authenticate_user!
 
@@ -12,20 +12,22 @@ class ReviewsController < ApplicationController
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
-
+  
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-
+    
     if @review.save
       redirect_to movie_path(@review.movie_id)
     else
       render 'new'
     end
   end
-
+  
   def update
+    @review = Review.find(params[:id])
     @review.update(review_params)
   end
 
