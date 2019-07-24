afba3d5368a527137d49345ccba0342023f75627
ASSROUGUI <ASSROUGUI@hotmail.com>
2019-07-24 16:58:58 +0300

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Movie.create(movie_name: "faten hamama", rating: 96, poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Fatin_hamama.jpg/220px-Fatin_hamama.jpg")
-Movie.create(movie_name: "Tsuad husni", rating: 118, poster: "https://i.pinimg.com/originals/f1/44/be/f144bea8c2e51b2a41d0daf10f74c597.jpg")
-Movie.create(movie_name: "Nelly", rating: 114,poster: "https://i.pinimg.com/736x/1c/31/9e/1c319e94dfc27b00eab297849631d612--egyptian-movies-egyptian-actress.jpg")
-Movie.create(movie_name: "marim fakhr Aldin ", rating: 79,poster: "https://i.pinimg.com/236x/44/19/d1/4419d11595a0b9c1ff94265dcea5403b--egyptian-movies-egyptian-actress.jpg")
-Movie.create(movie_name: "najla fathie", rating: 60, poster: "https://i.pinimg.com/originals/8d/b7/e3/8db7e3d27c20a3e10c7d6df8acdbd423.jpg")
